099420f4c79079856160689d1d249d68dfae3ce4
lhyqie <lhymvp@gmail.com>
2015-02-27 15:52:54 -0600

    Add hints to rotate2 and rotate3

--- src/array/RotateArray.java
+++ src/array/RotateArray.java
@@ -1,6 +1,5 @@
 package array;
 
-import utils.ArrayUtils;
 import static utils.ArrayUtils.print;   //static import for short hand
 
 /**
@@ -56,12 +55,13 @@ public class RotateArray {
 		int n = arr.length;
 		K %= n;   // if K > n, modulo K by n such that K is an integer within [0,n)
 		if(K == 0) return; // no need to rotate if K == 0
+		
 		int b[] = new int[K];
 		//step 1   b[0...K-1] <-- a[0...K-1]   (both of which are of size K)
 		for(int i = 0; i < K; i++){
 			b[i] = arr[i];
 		}
-		//step 2   a[0...n-K-1] <-- a[K...n-1] (both of which are of size N-K)
+		//step 2   a[0...n-K-1] <-- a[K...n-1] (both of which are of size n-K)
 		for (int i = 0; i < n-K; i++) {
 			arr[i] = arr[i+K];
 		}
@@ -77,19 +77,72 @@ public class RotateArray {
 	 * @param K: the number of positions to the left
 	 *  Time complexity O(n)
 	 *  Space complexity O(1)
+	 *  
+	 * Idea: for example a = {1,2,3,4,5}, shift K=2 positions to left
+	 * 		 imagine a sequence of array like a, 
+	 *       (1) each element of a after rotation a'[i] will either locate still at a or its predecessor if K < n
+	 *       ...{1 , 2, 3, 4, 5} {1, 2, 3, 4, 5} ...
+	 * 		                     /|\   /|\
+	 * 			        	      |     |  
+	 * 			                 i-k    i
+	 * 
+	 * 	     ...{1 , 2, 3, 4, 5} {1, 2, 3, 4, 5} ...
+	 * 		              /|\    /|\      /|\
+	 * 			           |	  |        |
+	 * 			           i-K    i      i-k+n
+	 * 			 We can use a simple formula to combine the two cases: 
+	 * 			 	a[(i-k+n)%n] <-- a[i]
+	 * 			 *** the element at position (i-k+n)%n of a is moved from position i of old a ***
+	 * 			
+	 *       (2) likewise, 
+	 *       		a[i] <-- a[(i+k)%n]
+	 *           **** the element at position i n of old a will be replaced by the element originally locate at (i+k)%n ***
+	 *      
+	 *       Since we are not allowed to use extra storage( namely, space complexity is O(1) ).
+	 *       So our algorithm uses a n-way swap: (It is a generalization of swapping two elements) 
+	 *       	t 			 	 <-- a[0]
+	 *       	a[0] 		 	 <-- a[(0+1*K)%n]
+	 *       	a[(0+1*K)%n] 	 <-- a[(0+2*K)%n]
+	 *       	a[(0+2*K)%n] 	 <-- a[(0+3*K)%n]
+	 *       	a[(0+3*K)%n] 	 <-- a[(0+4*K)%n]
+	 *       					 .
+	 *       					 .
+	 *       					 .
+	 *       	a[(0+(n-2)*K)%n] <-- a[(0+(n-1)*K)%n]
+	 *       	a[(0-k+n)%n] <-- t;
+	 *       Totally it swaps n+1 times
 	 */
 	public static void rotate2(int[] arr, int K){
+		if(K < 0)  throw new RuntimeException("K should be >= 0");  // K should not be negative
+		int n = arr.length;
+		K %= n;   // if K > n, modulo K by n such that K is an integer within [0,n)
+		if(K == 0) return; // no need to rotate if K == 0
+		
 		
 	}
 	
 	/**
 	 * Achieve rotation by reversing the array for three times
+	 * with the help of the function array.ReverseArray.reverseArray(...)
 	 * @param arr: the array to rotate
 	 * @param K: the number of positions to the left
 	 * Time complexity O(n)
 	 * Space complexity O(1)
+	 * 
+	 * Idea: For example a = {1,2,3,4,5}, shift K=2 positions to left 
+	 *     first reverse the entire array a
+	 *     		a' = {5, 4, 3, 2 ,1}
+	 *     divide a' into two parts whose size are (n-K) and K respectively
+	 *     		a_{1}' = {5, 4, 3}  a_{2}' = {2, 1}
+	 *     reverse a_{1}' and a_{2}' 
+	 *     		a_{1}'' = {3, 4, 5}  a_{2}'' = {1,2}
+	 *     a = a_{1}'' + a_{2}'' = {3, 4, 5, 1, 2}
+	 *       
 	 */
 	public static void rotate3(int[] arr, int K){
-		
+		if(K < 0)  throw new RuntimeException("K should be >= 0");  // K should not be negative
+		int n = arr.length;
+		K %= n;   // if K > n, modulo K by n such that K is an integer within [0,n)
+		if(K == 0) return; // no need to rotate if K == 0
 	}
 }
