0cbd2b9d89ae0db0e18d0fc2fd0c4caae052112b
lhyqie <lhymvp@gmail.com>
2015-04-04 16:20:07 -0500

    Add Problem datastream.WindowMax

--- src/datastream/WindowMax.java
+++ src/datastream/WindowMax.java
@@ -1,34 +1,34 @@
 package datastream;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
- * Online algorithm for computing the average most recent k elements of streaming data 
- * @author howie
- * Time complexity O(1) for each request
+ * Twitter Challenge, find the day with most tweets in recent k days 
+ * Also an interview question with Microsoft
  */
-public class WindowAverage {
-	
+public class WindowMax {
 	private int k; 			// window size
 	private DataStream ds;  // DataStream object
-	private float avg;      // current average
-	private int[] buffer;   // circular array of size k
 	private int t;          // current timestamp
-
+	private int[] buffer;   // circular array of size k, used for O(k) approach only
+	private Deque<Integer> Q; // double ended queue.  order from largest to smallest
+	
 	/**
 	 * @param k : window size k
 	 * @param ds : DataStream object
 	 */
-	public WindowAverage(int k, DataStream ds){
+	public WindowMax(int k, DataStream ds){
 		this.k = k;
 		this.ds = ds;
-		this.buffer = new int[k];
+		buffer = new int[k];
+		Q = new LinkedList<Integer>();
 	}
 	
 	public int getCurrentTimeStamp(){
 		return this.t;
 	}
-	/**
-	 * @return : the new element that received
-	 */
+	
 	public int getCurrent(){
 		int t = ds.getTimeStamp();
 		if(t == 0) throw new RuntimeException("fetch one from data stream first");
@@ -36,37 +36,32 @@ public class WindowAverage {
 	}
 	
 	/**
-	 * @return : the average of most recent k elements
+	 * @return : the max of most recent k elements
 	 * Time complexity O(1)
 	 */
-	public float getAverage(){
-		return avg;	
+	public int getMax(){
+		return buffer[Q.getFirst()%k];
 	}
+	
 	/**
-	 * @return : the average of most recent k elements
+	 * @return : the max of most recent k elements
 	 * Time complexity O(k)
 	 */
-	public float getAverageNaive(){
-		float avg = 0;
+	public int getMaxNaive(){
+		int max = 0;
 		int t = ds.getTimeStamp();
 		if(t < k){  // first k steps
-			for (int i = 0; i < t; i++) {
-				avg += buffer[i];
-			}
-			avg /= t;
+			for (int i = 0; i < t; i++) if( max < buffer[i]) max = buffer[i];
 		}else{
-			for (int i = 0; i < k; i++) {
-				avg += buffer[i];
-			}
-			avg /= k;
+			for (int i = 0; i < k; i++) if( max < buffer[i]) max = buffer[i];
 		}
-	
-		return avg;
+		return max;
 	}
 	
 	public int getWindowSize(){
 		return k;
 	}
+	
 	/**
 	 *  fetch data from data stream
 	 */
@@ -74,40 +69,55 @@ public class WindowAverage {
 		if(!ds.hasNext()) return;
 		this.t = ds.getTimeStamp();
 		int newData = ds.next();
+		//---------------- for naive approach -------------------
 		if(t < k){  // first k steps
 			buffer[t] = newData;
-			avg = (avg * t + buffer[t]) / (t + 1);
 		}else{
-			int oldData = buffer[t%k];
 			buffer[t%k] = newData;      //replace the old one
-			avg += (newData - oldData) * 1.0 / k;
 		}
+		//---------------- for advanced approach -------------------
+		if(t < k){  // first k steps
+			// delete elements smaller than newData because since newData is newer and larger, they have no chance to be max
+			while(!Q.isEmpty() && newData >= buffer[Q.getLast()%k]) 
+				Q.pollLast();
+			Q.addLast(t);
+		}else{
+			// delete elements smaller than newData because since newData is newer and larger, they have no chance to be max
+			while(!Q.isEmpty() && newData >= buffer[Q.getLast()%k]) 
+				Q.pollLast();
+			// if any element leaving the window, we update Q
+			while(!Q.isEmpty() && Q.getFirst() <= t-k) 
+				Q.pollFirst();
+			
+			Q.addLast(t);
+		}
+		
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("================================= Correctness Test  =====================================");
 		DataStream ds = new DataStream(10, 1, 10, 123434L);
 		System.out.println(ds);
-		WindowAverage wa = new WindowAverage(3, ds);
-		System.out.println("Window size k :" + wa.getWindowSize());
+		WindowMax wm = new WindowMax(3, ds);
+		System.out.println("Window size k :" + wm.getWindowSize());
 		while(ds.hasNext()){
-			wa.fetch();
-			System.out.println(String.format("t : %s \t element : %d \t naive average : %10.4f \taverage : %10.4f ",
-					wa.getCurrentTimeStamp(), wa.getCurrent(), wa.getAverageNaive(), wa.getAverage()));
+			wm.fetch();
+			System.out.println(String.format("t : %s \t element : %d \t naive average : %d \taverage : %d ",
+					wm.getCurrentTimeStamp(), wm.getCurrent(), wm.getMaxNaive(), wm.getMax()));
 		}
 		
 		System.out.println("\n\n================================= Performance Test  =====================================");
 		DataStream ds2 = new DataStream(1000000, 1, 10, 123434L);
-		WindowAverage wa2 = new WindowAverage(1000, ds2);
+		WindowMax wm2 = new WindowMax(1000, ds2);
 		
 		System.out.println("# of elements in the data stream : " + ds2.getLength());
-		System.out.println("Window size k :" + wa2.getWindowSize());
+		System.out.println("Window size k :" + wm2.getWindowSize());
 		System.out.println("O(1) approach : ");		
 		
 		long startTime = System.currentTimeMillis();
 		while(ds2.hasNext()){
-			wa2.fetch();
-			wa2.getAverage();
+			wm2.fetch();
+			wm2.getMax();
 		}
 		long endTime = System.currentTimeMillis();
 		System.out.println(" Time elapsed : " + (endTime - startTime) + " ms");
@@ -117,14 +127,13 @@ public class WindowAverage {
 		System.out.println("O(k) approach : ");
 		DataStream ds3 = new DataStream(10000000, 1, 10, 123434L);
 		//System.out.println(ds2);
-		WindowAverage wa3 = new WindowAverage(10000, ds3);
+		WindowMax wm3 = new WindowMax(10000, ds3);
 		startTime = System.currentTimeMillis();
 		while(ds3.hasNext()){
-			wa3.fetch();
-			wa3.getAverageNaive();
+			wm3.fetch();
+			wm3.getMaxNaive();
 		}
 		endTime = System.currentTimeMillis();
 		System.out.println(" Time elapsed : " + (endTime - startTime) + " ms");
 	}
 }
- 
