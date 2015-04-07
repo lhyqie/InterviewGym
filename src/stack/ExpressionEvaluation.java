c2029d575299eb41292f7a6b8388b49c45c0cc9c
lhyqie <lhymvp@gmail.com>
2015-04-07 14:27:00 -0500

    Add Problem ExpressionEvaluation

---
+++ src/stack/ExpressionEvaluation.java
@@ -0,0 +1,29 @@
+package stack;
+
+public class ExpressionEvaluation {
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * @param expression : Expression can contain parentheses, 
+	 * parentheses are well-matched. For simplicity, 
+	 * allowed are +, -, *, and /
+	 * @return
+	 */
+	public static int intEvaluate(String expression){
+		return 0;
+	}
+	
+	/**
+	 * @param expression : Expression can contain parentheses, 
+	 * parentheses are well-matched. For simplicity, 
+	 * allowed are +, -, *, and /
+	 * numbers can contain . 
+	 * @return
+	 */
+	public static double doubleEvaluate(String expression){
+		return 0;
+	}
+	
+}
