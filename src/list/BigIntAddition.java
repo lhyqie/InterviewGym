557a0e85e2f2f90db6d5fdc69d33d126d5f26bbb
lhyqie <lhymvp@gmail.com>
2015-04-11 08:59:24 -0500

    Add Problem BigIntAddition

---
+++ src/list/BigIntAddition.java
@@ -0,0 +1,57 @@
+package list;
+
+class BigInt{
+	
+	private MyList digits = null;
+	
+	public BigInt(){
+		digits = new MyList(new MyList.Node(100));
+	}
+	
+	public BigInt(String aLargeNumber){
+		digits = new MyList();
+		for(int i = 0; i < aLargeNumber.length(); i++){
+			MyList.Node d = new MyList.Node(aLargeNumber.charAt(i));
+			d.next = digits.head;
+			digits.head = d;
+		}
+	}
+	
+	public String toString(){
+		StringBuffer sb = new StringBuffer();
+		MyList.Node p = digits.head;
+		while(p!=null){
+			sb.append((int)(p.e) - 48);
+			p=p.next;
+		}
+		return sb.reverse().toString();
+	}
+
+	/**
+	 * add two big Int
+	 * (list based addition)
+	 * @param other
+	 * @return
+	 */
+	public BigInt add(BigInt other) {
+		BigInt ret = new BigInt();
+		
+		return ret;
+	}
+}
+
+
+public class BigIntAddition {
+	
+	public static void main(String[] args) {
+		BigInt a = new BigInt("123456789123456789");
+		System.out.println(a);
+		
+		BigInt b = new BigInt("11111111111111111111");
+		System.out.println(b);
+		
+		BigInt c = a.add(b);
+		
+	}
+	
+}
