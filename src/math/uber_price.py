f9e37e422254d19760c2f27e3dcea6bdae86c5b3
lhyqie <lhymvp@gmail.com>
2015-04-14 22:14:06 -0500

    Fix uber_price

--- src/math/uber_price.py
+++ src/math/uber_price.py
@@ -6,8 +6,8 @@ Price experiment
 Uber is a company based in San Francisco, California that makes a mobile application that connects passengers with drivers of luxury vehicles for hire.
 Uber is doing experiments to better understand the relationship between price and demand for rides. One hypothesis is that lower prices will show an increase in demand for cars.
 Their goal is to find the cost/demand point that is best for everyone.
-Experimental data will show that the relation between the price discount and the uber revenue can be approximated by a mathematical function. Let¡¯s consider the following function:
-y = 1/2 (¡Ì(-3x^2 + Ax + B) - x - 1)
+Experimental data will show that the relation between the price discount and the uber revenue can be approximated by a mathematical function. Let's consider the following function:
+y = 1/2 (sqrt(-3x^2 + Ax + B) - x - 1)
 where
 x represents the discount,
 y represents the uber revenue and
