f5bd9763210c70bef20e351e6abba2dfb9385982
lhyqie <lhymvp@gmail.com>
2015-04-16 17:01:37 -0500

    Add solution to BigIntAddition

--- src/list/BigIntAddition.java
+++ src/list/BigIntAddition.java
@@ -11,7 +11,7 @@ class BigInt{
 	public BigInt(String aLargeNumber){
 		digits = new MyList();
 		for(int i = 0; i < aLargeNumber.length(); i++){
-			MyList.Node d = new MyList.Node(aLargeNumber.charAt(i));
+			MyList.Node d = new MyList.Node(aLargeNumber.charAt(i) - 48);
 			d.next = digits.head;
 			digits.head = d;
 		}
@@ -21,7 +21,7 @@ class BigInt{
 		StringBuffer sb = new StringBuffer();
 		MyList.Node p = digits.head;
 		while(p!=null){
-			sb.append((int)(p.e) - 48);
+			sb.append(p.e);
 			p=p.next;
 		}
 		return sb.reverse().toString();
@@ -35,7 +35,54 @@ class BigInt{
 	 */
 	public BigInt add(BigInt other) {
 		BigInt ret = new BigInt();
+		MyList.Node p1 = this.digits.head;
+		MyList.Node p2 = other.digits.head;
+		ret.digits.head = new MyList.Node(-999); // dummy node
+		MyList.Node p3 = ret.digits.head;
+		int c = 0;
+		while(p1 != null && p2 != null){
+			int sum = p1.e + p2.e + c;
+			if( sum >= 10){
+				sum %= 10;
+				c = 1;
+			}else{
+				c = 0;
+			}
+			p1 = p1.next;  p2 = p2.next;
+			p3.next = new MyList.Node(sum);
+			p3 = p3.next;
+		}
+		while(p1 != null){ // if list 1 is longer
+			int sum = p1.e+ c;
+			if( sum >= 10){
+				sum %= 10;
+				c = 1;
+			}else{
+				c = 0;
+			}
+			p1 = p1.next;
+			p3.next = new MyList.Node(sum);
+			p3 = p3.next;
+		}
+		while(p2 != null){ // if list 2 is longer
+			int sum = p2.e + c;
+			if( sum >= 10){
+				sum %= 10;
+				c = 1;
+			}else{
+				c = 0;
+			}
+			p2 = p2.next;
+			p3.next = new MyList.Node(sum);
+			p3 = p3.next;
+		}
+		//don't forget the carry
+		if( c == 1){
+			p3.next = new MyList.Node(c);
+			p3 = p3.next;
+		}
 		
+		ret.digits.head = ret.digits.head.next;  // throw the dummy node
 		return ret;
 	}
 }
@@ -44,14 +91,24 @@ class BigInt{
 public class BigIntAddition {
 	
 	public static void main(String[] args) {
-		BigInt a = new BigInt("123456789123456789");
-		System.out.println(a);
 		
-		BigInt b = new BigInt("11111111111111111111");
-		System.out.println(b);
+		String[] As = {"5","1","","1"};
+		String[] Bs = {"5","" ,"1","9999"};
 		
-		BigInt c = a.add(b);
-		System.out.println(c);
+		for (int i = 0; i < Bs.length; i++) {
+			i = 3;
+			BigInt a = new BigInt(As[i]);
+			System.out.println("a = " + a);
+			
+			BigInt b = new BigInt(Bs[i]);
+			System.out.println("b = " + b);
+			
+			BigInt c = a.add(b);
+			System.out.println("c = " + c);
+			System.out.println();
+		}
+		
+		
 	}
 	
 }
