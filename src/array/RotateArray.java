05bbcec8e6fa1c6d6a8e42fdec098dd940d463db
lhyqie <lhymvp@gmail.com>
2015-12-02 15:18:23 -0600

    fix a bug

--- src/array/RotateArray.java
+++ src/array/RotateArray.java
@@ -34,12 +34,6 @@ public class RotateArray {
 		print(copy2);
 		System.out.println();
 		
-		int [] copy3 = origin.clone();
-		System.out.print("Array copy3 before applying rotate3 :\t");
-		print(copy3);
-		rotate3(copy3, K);
-		System.out.print("Array copy3 after applying rotate3 :\t");
-		print(copy3);
 	}
 	
 	
@@ -72,60 +66,6 @@ public class RotateArray {
 	}
 	
 	/**
-	 * Swap elements directly without extra storage
-	 * @param arr: the array to rotate
-	 * @param K: the number of positions to the left
-	 *  Time complexity O(n)
-	 *  Space complexity O(1)
-	 *  
-	 * Idea: for example a = {1,2,3,4,5}, shift K=2 positions to left
-	 * 		 imagine a sequence of array like a, 
-	 *       (1) each element of a after rotation a'[i] will either locate still at a or its predecessor if K < n
-	 *       ...{1 , 2, 3, 4, 5} {1, 2, 3, 4, 5} ...
-	 * 		                     /|\   /|\
-	 * 			        	      |     |  
-	 * 			                 i-k    i
-	 * 
-	 * 	     ...{1 , 2, 3, 4, 5} {1, 2, 3, 4, 5} ...
-	 * 		              /|\    /|\      /|\
-	 * 			           |	  |        |
-	 * 			           i-K    i      i-k+n
-	 * 			 We can use a simple formula to combine the two cases: 
-	 * 			 	a[(i-k+n)%n] <-- a[i]
-	 * 			 *** the element at position (i-k+n)%n of a is moved from position i of old a ***
-	 * 			
-	 *       (2) likewise, 
-	 *       		a[i] <-- a[(i+k)%n]
-	 *           **** the element at position i n of old a will be replaced by the element originally locate at (i+k)%n ***
-	 *      
-	 *       Since we are not allowed to use extra storage( namely, space complexity is O(1) ).
-	 *       So our algorithm uses a n-way swap: (It is a generalization of swapping two elements) 
-	 *       	t 			 	 <-- a[0]
-	 *       	a[0] 		 	 <-- a[(0+1*K)%n]
-	 *       	a[(0+1*K)%n] 	 <-- a[(0+2*K)%n]
-	 *       	a[(0+2*K)%n] 	 <-- a[(0+3*K)%n]
-	 *       	a[(0+3*K)%n] 	 <-- a[(0+4*K)%n]
-	 *       					 .
-	 *       					 .
-	 *       					 .
-	 *       	a[(0+(n-2)*K)%n] <-- a[(0+(n-1)*K)%n]
-	 *       	a[(0-k+n)%n] <-- t;
-	 *       Totally it swaps n+1 times
-	 */
-	public static void rotate2(int[] arr, int K){
-		if(K < 0)  throw new RuntimeException("K should be >= 0");  // K should not be negative
-		int n = arr.length;
-		K %= n;   // if K > n, modulo K by n such that K is an integer within [0,n)
-		if(K == 0) return; // no need to rotate if K == 0
-		int t = arr[0];                  // first swap, save value of a[0]
-		for (int c = 0; c < n-1; c++) {  // perform n-1 time swaps
-			arr[(c*K)%n] = arr[((c+1)*K)%n];
-		}
-		arr[(-K+n)%n] = t;				 // last swap, 	a[(0-k+n)%n] <-- a[0] 
-		
-	}
-	
-	/**
 	 * Achieve rotation by reversing the array for three times
 	 * with the help of the function array.ReverseArray.reverseArray(...)
 	 * @param arr: the array to rotate
@@ -143,7 +83,7 @@ public class RotateArray {
 	 *     a = a_{1}'' + a_{2}'' = {3, 4, 5, 1, 2}
 	 *       
 	 */
-	public static void rotate3(int[] arr, int K){
+	public static void rotate2(int[] arr, int K){
 		if(K < 0)  throw new RuntimeException("K should be >= 0");  // K should not be negative
 		int n = arr.length;
 		K %= n;   // if K > n, modulo K by n such that K is an integer within [0,n)
